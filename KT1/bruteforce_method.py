7788987a6ca564e957865c5f95a8ee9b27674690
Krearden <->
2021-12-27 16:04:46 +0500

    change directory of kt1

--- KT1/bruteforce_method.py
+++ KT1/bruteforce_method.py
@@ -11,12 +11,12 @@ def bruteforce_method(function_number):
     n = length / 0.01
     n = int(n)
     minimum_value = math.inf
-    minimum_x = None
     for i in range(n):
         X_i = A + i * (length / n)
         value_of_X_i = function.getFunctionValue(X_i, function_number)
         if value_of_X_i < minimum_value:
             minimum_value = value_of_X_i
             minimum_x = X_i
+    print(minimum_x, minimum_value)
     print(f'[МЕТОД ПЕРЕБОРА] x_min = {minimum_x}, total iterations = {n}')
 
